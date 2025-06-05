3d14684137336532e3547576c79969c4aa037806
ctynt <2597704619@qq.com>
2025-06-05 16:14:05 +0800

    iot模块命令发送修改

--- iot-cloud-iot/src/main/java/top/dl/controller/CommandController.java
+++ iot-cloud-iot/src/main/java/top/dl/controller/CommandController.java
@@ -1,8 +1,12 @@
 package top.dl.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import top.dl.framework.common.utils.Result;
 import top.dl.service.CommandService;
 
@@ -11,14 +15,18 @@ import top.dl.service.CommandService;
  * @Date 2025/6/4
  * @Description CommandController
  **/
-
+@RestController
+@RequestMapping("api/command")
+@Tag(name = "命令模块")
+@AllArgsConstructor
 public class CommandController {
-    private CommandService commandService;
+    private final CommandService commandService;
 
     @PostMapping("/control")
     @Operation(summary = "发送控制命令")
     public Result<String> sendCommand(@RequestParam String deviceId, @RequestParam String command) {
         commandService.sendCommand(deviceId, command);
+        System.out.print("fasongchengg");
         return Result.ok("指令发送成功！");
     }
 }
--- iot-cloud-iot/src/main/java/top/dl/service/CommandService.java
+++ iot-cloud-iot/src/main/java/top/dl/service/CommandService.java
@@ -1,11 +1,14 @@
 package top.dl.service;
 
+import top.dl.entity.Device;
+import top.dl.framework.mybatis.service.BaseService;
+
 /**
  * @Author ctynt
  * @Date 2025/6/4
  * @Description CommandService
  **/
 
-public interface CommandService {
+public interface CommandService extends BaseService<Device> {
     void sendCommand(String deviceId, String command);
 }
